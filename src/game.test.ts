87cb14aa3dd724b525d8d364f212db24488c0dab
draberf <draberf@gmail.com>
2023-03-18 12:31:09 +0100

    Add testing for overlap function

--- src/game.test.ts
+++ src/game.test.ts
@@ -25,3 +25,48 @@ test('vertical carpet second tile has good offset', () => {
     let carpet: Carpet = new Carpet(4,4,true);
     expect(carpet.getSecondTile()).toEqual({x: 4, y: 5});
 })
+
+test('carpets overlap or don\'t', () => {
+    // create a list of carpets one way or another
+    let carpets: Array<Carpet> = [];
+    carpets.push(new Carpet(0,0,false));
+    carpets.push(new Carpet(1,0,false));
+    carpets.push(new Carpet(2,0,false));
+    carpets.push(new Carpet(0,1,false));
+    carpets.push(new Carpet(0,-1, false));
+
+    carpets.push(new Carpet(0,0,true));
+    carpets.push(new Carpet(0,1,true));
+    carpets.push(new Carpet(0,2,true));
+    carpets.push(new Carpet(1,0,true));
+    carpets.push(new Carpet(-1,0,true));
+    
+    // create expected pairings
+    let pairings: Array<Array<boolean>> = [
+        // horizontal                 |      vertical
+        [true,  false,  false,  false,  true,   false,  false,  true,   false],
+        [       true,   false,  false,  false,  false,  false,  true,   false],
+        [               false,  false,  false,  false,  false,  false,  false],
+        [                       false,  true,   true,   false,  true,   false],
+        [                               false,  false,  false,  false,  false],
+        [                                       true,   false,  false,  false],
+        [                                               true,   false,  false],
+        [                                                       false,  false],
+        [                                                               false]
+    ]
+
+    let carpet_count: number = carpets.length;
+    if (pairings.length != carpet_count-1) {
+        throw Error("Not one fewer expected pairings than carpets.");
+    }
+    // TODO: check for all pairings
+
+    for (let i = 0; i < carpet_count; i++) {
+        for (let ii = i+1; ii < carpet_count; ii++) {
+            expect([i, ii, checkOverlap(carpets[i], carpets[ii])]).toEqual([i, ii, pairings[i][ii-i-1]]);
+            expect([i, ii, checkOverlap(carpets[ii], carpets[i])]).toEqual([i, ii, pairings[i][ii-i-1]]);
+        }
+    }
+})
+
+// 
