64def8cd815544705182ae5f8360126d10f45e7e
draberf <draberf@gmail.com>
2023-04-19 16:49:08 +0200

    Work on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { EventHandler } from 'react';
+import React, { EventHandler, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BindingElement } from 'typescript';
@@ -18,8 +18,9 @@ import yellow_full from './assets/carpets/yellow_complete.png'
 import empty from './assets/carpets/empty.png'
 
 import arc from './assets/arc.png'
-import arrow from './assets/arrow.png'
 import assam from './assets/assam.png'
+import pointer from './assets/pointer.png'
+import dirham from './assets/dirham.png'
 
 const colors_half = [
   blue_half, orange_half, red_half, yellow_half
@@ -28,6 +29,11 @@ const colors_full = [
   blue_full, orange_full, red_full, yellow_full
 ];
 
+function GetDirectionalTransform(direction: Direction): string {
+  return 'rotate('+Array(180,90,0,270)[direction]+'deg)';
+}
+
+/*
 function App() {
   return (
     <div className="App">
@@ -48,6 +54,7 @@ function App() {
     </div>
   );
 }
+*/
 
 type GameObjectProp = {
   game: Game;
@@ -60,11 +67,33 @@ type TileProp = {
 }
 
 function GameWindow({ game }: GameObjectProp) {
-  return <>
-    <Board game={game} />
-    <PlayerArea game={game} />
-    <OpponentsArea game={game} />
-  </>
+
+  const [gameState, setGameState] = useState(game);
+
+  return <div className='gameWindow'>
+    <GameArea game={gameState} />
+    <PlayerArea game={gameState} />
+    <OpponentsArea game={gameState} />
+  </div>
+}
+
+type ArrowProp = {
+  game: Game;
+  direction: Direction;
+}
+
+function GameArea({game}: GameObjectProp) {
+  return <div className='gameArea'>
+    <Board game={game}/>
+    <Arrow game={game} direction={Direction.NORTH}/>
+    <Arrow game={game} direction={Direction.EAST}/>
+    <Arrow game={game} direction={Direction.WEST}/>
+    <Arrow game={game} direction={Direction.SOUTH}/>
+  </div>
+}
+
+function Arrow({game, direction}: ArrowProp) {
+  return <img src={pointer} className={'arrow arrow--'+direction}/>
 }
 
 function Board({ game }: GameObjectProp) {
@@ -76,15 +105,40 @@ function Board({ game }: GameObjectProp) {
     }
   }
 
-  return <div className='boardGrid'>
-    {tiles}
+  return <div className='boardArea'>
+    <div className='boardGrid'>
+      {tiles}
+    </div>
   </div>
 }
 
 function Tile({ game, coordX, coordY }: TileProp) {
   if (coordX < 0 || coordY < 0 || coordX >= 7 || coordY >= 7) {
+
+    let arcDir: string = 'rotate(270deg)';
+    if (coordY < 0 && coordX < 7) {
+      arcDir = coordX % 2 ? 'rotate(0deg)' : 'rotate(90deg)';
+    }
+    if (coordX >= 7 && coordY < 7) {
+      arcDir = coordY % 2 ? 'rotate(180deg)' : 'rotate(90deg)';
+    }
+    if (coordY >= 7 && coordX >= 0) {
+      arcDir = coordX % 2 ? 'rotate(180deg)' : 'rotate(270deg)';
+    }
+    if (coordX < 0 && coordY >= 0) {
+      arcDir = coordY % 2 ? 'rotate(0deg)' : 'rotate(270deg)'
+    }
+
+    // override corners
+    if (coordX == 7 && coordY == -1) {
+      arcDir = 'rotate(270deg)';
+    }
+    if (coordX == -1 && coordY == 7) {
+      arcDir = 'rotate(90deg)';
+    }
+
     return <div key={(coordY*9 + coordX).toString()} className="tile">
-      <img src={arc} className='floor' />
+      <img src={arc} className='floor' style={{transform: arcDir}}/>
     </div>;
   }
 
@@ -93,6 +147,8 @@ function Tile({ game, coordX, coordY }: TileProp) {
 
   let floorSrc = empty;
   const color = game.board.color(coordX, coordY);
+  const dir = game.board.direction(coordX, coordY);
+  const dirTransform: string = GetDirectionalTransform(dir);
 
 
   if (color) {
@@ -100,9 +156,14 @@ function Tile({ game, coordX, coordY }: TileProp) {
   }
   
   if (game.board.assam_x == coordX && game.board.assam_y == coordY) {
-    content = <><img src={floorSrc} className='floor'/><img src = {assam} className='assam' /></>;
+    const assamDir = game.board.assam_dir;
+    const assamTransform: string = GetDirectionalTransform(assamDir);
+    content = <>
+      <img src={floorSrc} className='floor' style={{transform:dirTransform}}/>
+      <img src = {assam} className='assam' style={{transform:assamTransform}}/>
+    </>;
   } else {
-    content = <img src={floorSrc} className='floor'/>;
+    content = <img src={floorSrc} className='floor' alt={dirTransform} style={{transform:dirTransform}}/>;
   }
 
   return <div key={(coordY*9 + coordX).toString()} className="tile">{content}</div>;
@@ -111,20 +172,25 @@ function Tile({ game, coordX, coordY }: TileProp) {
 function PlayerArea({ game }: GameObjectProp) {
   // TODO: track CURRENTLY displayed player
 
-  return <>
+  return <div className='playerArea'>
     <b>Player Area:</b>
-    <img src={red_half} style={{width:"10%"}}/>
+    <FullCarpet/>
+
     <button>Roll</button>
     <div>Dirhams: {game.players[0].dirhams}</div>
-  </>
+  </div>
 }
 
 function OpponentsArea({ game }: GameObjectProp) {
-  return <>
+  return <div className='oppArea'>
   <b>Opponents:</b>
-  <img src={blue_half} style={{width:"10%"}} />
+  <img src={blue_half} style={{width:"10px"}} />
   {game.players[1].dirhams}
-  </>
+  </div>
+}
+
+function FullCarpet() {
+  return <img src={red_full}/>
 }
 
 // change this
@@ -135,6 +201,11 @@ export default function MockApp() {
   ]);
 
   game.board.placeCarpet(new Carpet(4,5,true,Color.RED));
+  game.board.moveAssam(2);
+  game.board.turnAssam(true);
+
+  game.board.placeCarpet(new Carpet(0,0,false,Color.BLUE));
+  game.board.placeCarpet(new Carpet(2,5,false,Color.RED));
 
   return <GameWindow game={game} />
 }
